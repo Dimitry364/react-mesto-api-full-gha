61a092d85fc68544cd90e0261f25603b7b1fcb4d
Dimitry364 <g4779@yandex.ru>
2024-02-05 00:06:00 +0700

    Правки

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,25 +1,26 @@
-const jwt = require("jsonwebtoken");
-const AuthError = require("../errors/AuthError");
-const { jwtKey } = require("../utils/jwtKey");
+const jwt = require('jsonwebtoken');
+const AuthError = require('../errors/AuthError');
+const { jwtKey } = require('../utils/jwtKey');
+
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(401).send({ message: "Необходима авторизация" });
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    next(new AuthError('Необходима авторизация'));
   }
 
-  const token = authorization.replace("Bearer ", "");
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(
       token,
-      NODE_ENV === "production" ? JWT_SECRET : jwtKey
+      NODE_ENV === 'production' ? JWT_SECRET : jwtKey,
     );
   } catch (err) {
-    next(new AuthError("Необходима авторизация"));
+    next(new AuthError('Необходима авторизация'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,22 +1,22 @@
 const allowedCors = [
-  "https://yarushkin.practicum.nomoredomainsmonster.ru",
-  "http://yarushkin.practicum.nomoredomainsmonster.ru",
-  "http://localhost:3000",
+  'https://yarushkin.practicum.nomoredomainsmonster.ru',
+  'http://yarushkin.practicum.nomoredomainsmonster.ru',
+  'http://localhost:3000',
 ];
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers["access-control-request-headers"];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
-  if (method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-    res.header("Access-Control-Allow-Headers", requestHeaders);
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
 
